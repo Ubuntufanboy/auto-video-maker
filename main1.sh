3919761ed184b8fbb102be65fef51328c490367a
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-07-22 12:56:07 -0400

    Added welcome msg and added more info
    I also removed a typo

--- main1.sh
+++ main1.sh
@@ -1,2 +1,4 @@
+echo '----- Welcome to the auto-video-maker script! -----'
 python3 yt-download.py
-echo 'Go turn input.py into an mp3 file then run main2.sh'
+echo 'Go turn  the .opus file into an mp3 file with ffmpeg then run main2.sh'
+echo 'run ffmpeg [[filename.opus]] -i input.mp3'
