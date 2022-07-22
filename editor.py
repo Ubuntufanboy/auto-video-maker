9b624f9edf603d0c9483ec4455666835f02def1d
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-07-22 12:54:55 -0400

    Added termcolor to editor.py

--- editor.py
+++ editor.py
@@ -1,5 +1,9 @@
-print("Welcome to the editing app!")
-print("enter a value 0.5 - 2.0 to change the speed by")
+from termcolor import colored
+
+msg = colored("editor", "green")
+msg = f"----- Welcome to the audio {msg}! You can change the speed of the audio here -----"
+print(msg)
+print("enter a value from 0.5 - 2.0 to change the speed by (1.0 will make it not change)")
 print("")
 value = float(input("Enter here: -> "))
 from os import system
