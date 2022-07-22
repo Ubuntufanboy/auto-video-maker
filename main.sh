276cf9765261ea44f2d4cb478139327ca07b3272
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-07-22 17:32:04 -0400

    Created main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+echo '----- Welcome to the auto-video-maker script! -----'
+python3 yt-download.py
+echo ''
+echo ''
+echo '----- INSTRUCTIONS -----'
+echo ''
+echo 'run ffmpeg -i [[filename.opus]] input.mp3'
+echo 'then run python3 editor.py'
