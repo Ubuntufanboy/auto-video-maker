f133c98455e7b01d28bc995e66e3de71b2112182
Ubuntufanboy <82414370+Ubuntufanboy@users.noreply.github.com>
2022-07-24 00:16:12 -0400

    Version 1.2 is here!
    Look in release notes for more info

--- main.sh
+++ main.sh
@@ -1,8 +1,3 @@
 echo '----- Welcome to the auto-video-maker script! -----'
 python3 yt-download.py
-echo ''
-echo ''
-echo '----- INSTRUCTIONS -----'
-echo ''
-echo 'run ffmpeg -i [[filename.opus]] input.mp3'
-echo 'then run python3 editor.py'
+python3 editor.py
